8c931a7c08e8c6183ebabf8a1a1cf2c64c895f7a
hetavip227-ui <hetavip227@gmail.com>
2026-01-19 22:53:30 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a,b=5,10
+>>> print(a+b)
+15
+>>> a=2
+>>> b=8
+>>> print(a-b)
+-6
+>>> a,b=5,7
+>>> print(a*b)
+35
+>>> a=9
+>>> b=3
+>>> print(a/b)
+3.0
