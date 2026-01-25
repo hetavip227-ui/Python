1160608ab72d196713ee54121fcb1ae7268a4bd9
hetavip227-ui <hetavip227@gmail.com>
2026-01-25 17:44:28 +0530

    Add files via upload

---
+++ area perimter comparison.py
@@ -0,0 +1,7 @@
+l,b=int(input("enter value")),int(input("enter value"))
+area=l*b
+perimeter=2*(l+b)
+if(area>perimeter):
+    print(area,",area is greater")
+else:
+    print(perimeter,",perimeter is greater")
