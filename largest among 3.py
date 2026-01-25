b5a01d48f93daeb74d7ba653928fbf9dd712634e
hetavip227-ui <hetavip227@gmail.com>
2026-01-25 17:16:52 +0530

    Add files via upload

---
+++ largest among 3.py
@@ -0,0 +1,7 @@
+a,b,c=int(input("enter value")),int(input("enter value")),int(input("enter value"))
+if(a>b>c):
+    print(a,"is largest")
+elif(b>c>a):
+    print(b,"is largest")
+elif(c>a>b):
+    print(c,"is largest")
